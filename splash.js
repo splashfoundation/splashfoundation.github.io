c61db46a9bea320a60ef630b0eef6d99a3dee5b5
ScreenyMcScreenface\Ryan <carpiediem@gmail.com>
2017-12-24 23:13:05 +0800

    added click events to buttons

--- splash.js
+++ splash.js
@@ -1,5 +1,14 @@
 $(function(){
   $('#format-selector').change(function(){
     console.log($(this)[0].value);
-  })
+  });
+
+  $('#weeks button').click(function(){
+    $('body').attr('data-week', $(this).attr('data-week'));
+  });
+
+  $('#groups button').click(function(){
+    $('body').attr('data-group', $(this).attr('data-group'));
+  });
+
 });
